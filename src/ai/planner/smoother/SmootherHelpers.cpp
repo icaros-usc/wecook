78821328fd5a732b19cd287b2dacb9a7cacb2fc0
Hejia Zhang <hjzh578@gmail.com>
2019-11-24 17:31:53 -0800

    quick fix wecook#10 by unrolling continuous joint values right before calling smoothing functions

--- src/ai/planner/smoother/SmootherHelpers.cpp
+++ src/ai/planner/smoother/SmootherHelpers.cpp
@@ -1,11 +1,107 @@
 //
 // Created by hejia on 9/16/19.
 //
+#include <aikido/statespace/dart/SO2Joint.hpp>
 #include "SmootherUtil.h"
 #include "ai/planner/smoother/SmootherHelpers.h"
 
 namespace wecook {
 namespace planner {
+/*
+ * Dangerous
+ */
+void unroll(Eigen::MatrixXd &traj) {
+  enum FLIP_TYPE {
+    /// -pi to pi
+    NEGATIVE,
+    /// pi to -pi
+    POSITIVE,
+  };
+
+  std::size_t cols = traj.cols();
+  std::size_t rows = traj.rows();
+
+  for (std::size_t col = 0; col < cols; ++col) {
+    std::vector<std::pair<std::size_t, FLIP_TYPE >> flips{};
+    Eigen::VectorXd colVec = traj.col(col);
+    auto last = colVec(0);
+    for (std::size_t row = 1; row < rows; ++row) {
+      if (colVec(row) * last < 0) {
+        // Check if it's a flip, i.e., from -pi to pi or pi to -pi
+        if (last < 0) {
+          if (fabs(colVec(row)) + fabs(last) > fabs(colVec(row) - M_PI) + fabs(last + M_PI)) {
+            flips.emplace_back(std::pair<std::size_t, FLIP_TYPE >{row, NEGATIVE});
+          }
+        } else {
+          if (fabs(colVec(row)) + fabs(last) > fabs(colVec(row) + M_PI) + fabs(last - M_PI)) {
+            flips.emplace_back(std::pair<std::size_t, FLIP_TYPE >{row, POSITIVE});
+          }
+        }
+      }
+      last = colVec(row);
+    }
+    // Unpate joint values based on flips
+    for (const auto &flip : flips) {
+      auto startIndex = flip.first;
+      Eigen::VectorXd addVec(rows - startIndex);
+      if (flip.second == NEGATIVE) {
+        addVec.setConstant(-2 * M_PI);
+      } else {
+        addVec.setConstant(2 * M_PI);
+      }
+      // Update
+      traj.col(col).segment(startIndex, rows - startIndex) += addVec;
+    }
+  }
+}
+
+/*
+ * Dangerous
+ */
+aikido::trajectory::InterpolatedPtr unroll(const aikido::statespace::dart::MetaSkeletonStateSpacePtr &stateSpace,
+                                           const aikido::trajectory::Trajectory * originalTraj) {
+  auto originalInterpolated = dynamic_cast<const aikido::trajectory::Interpolated *>(originalTraj);
+
+  if (!originalInterpolated) {
+    std::__throw_invalid_argument(
+        "[SmootherHelpers::unroll]: The trajectory to be preprocessed should be interpolated!");
+  }
+
+  std::shared_ptr<aikido::statespace::GeodesicInterpolator>
+      interpolator = std::make_shared<aikido::statespace::GeodesicInterpolator>(stateSpace);
+  auto newInterpolated = std::make_shared<aikido::trajectory::Interpolated>(stateSpace, interpolator);
+
+  std::size_t numWaypoints = originalInterpolated->getNumWaypoints();
+  Eigen::MatrixXd traj(numWaypoints, stateSpace->getDimension());
+  for (std::size_t i = 0; i < numWaypoints; ++i) {
+    Eigen::VectorXd temp;
+    stateSpace->logMap(originalInterpolated->getWaypoint(i), temp);
+    traj.row(i) = temp;
+  }
+
+  unroll(traj);
+
+  const auto n = stateSpace->getNumSubspaces();
+  for (std::size_t i = 0; i < numWaypoints; ++i) {
+    auto newState = stateSpace->createState();
+    size_t index = 0;
+    for (std::size_t j = 0; j < n; ++j) {
+      auto subSpace = stateSpace->getSubspace<aikido::statespace::dart::JointStateSpace>(j);
+      auto dim = subSpace->getDimension();
+      auto newSubState = stateSpace->getSubState<>(newState, j);
+      if (dynamic_cast<const aikido::statespace::dart::SO2Joint *>(subSpace.get())) {
+        dynamic_cast<const aikido::statespace::dart::SO2Joint *>(subSpace.get())->expMapUnbounded(traj.row(i).segment(index, dim), newSubState);
+      } else {
+        subSpace->expMap(traj.row(i).segment(index, dim), newSubState);
+      }
+      index += dim;
+    }
+    newInterpolated->addWaypointUnbounded(i, newState);
+  }
+
+  return newInterpolated;
+}
+
 /*
  * Simple short cutting
  */
@@ -178,17 +274,19 @@ std::unique_ptr<ai::trajectory::HauserPath> hauserSmoothPathHauserPath(const std
                                                                        const aikido::constraint::TestablePtr &constraint) {
   Eigen::VectorXd velocityLimits = ada->getVelocityLimits(metaSkeletonPtr);
   Eigen::VectorXd accelerationLimits = ada->getAccelerationLimits(metaSkeletonPtr);
-  auto interpolated = dynamic_cast<const aikido::trajectory::Interpolated *>(path);
-  if (!interpolated) {
-    auto spline = dynamic_cast<const aikido::trajectory::Spline *>(path);
-    if (!spline) {
-      throw std::invalid_argument("Path should be either spline or Interpolated.");
-    }
-    interpolated = detail::SplineToInterpolated(*spline);
-  }
+//  auto interpolated = dynamic_cast<const aikido::trajectory::Interpolated *>(path);
+//  if (!interpolated) {
+//    auto spline = dynamic_cast<const aikido::trajectory::Spline *>(path);
+//    if (!spline) {
+//      throw std::invalid_argument("Path should be either spline or Interpolated.");
+//    }
+//    interpolated = detail::SplineToInterpolated(*spline);
+//  }
+  // before smoothing, we will add pi to each SO2 joint, so there will be no wield rotation.
+  auto processedTraj = unroll(metaStateSpace, path);
   return hauserDoShortcutHauserPath(metaStateSpace,
                                     metaSkeletonPtr,
-                                    *interpolated,
+                                    *processedTraj,
                                     *(ada->cloneRNG().get()),
                                     constraint,
                                     velocityLimits,
