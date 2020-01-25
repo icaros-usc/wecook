70b02b61ea260d076308456e6485407cafaaca30
Pradeep Kadubandi <pradeep.v.kadubandi@gmail.com>
2020-01-25 13:07:38 -0800

    Defining a Tag class (similar to Object class) to hold TagMsg and integrating it with Task / TaskManager to make it available for rest of the system.

---
+++ include/wecook/Tag.h
@@ -0,0 +1,44 @@
+//
+// Wecook licence info!
+//
+
+#ifndef WECOOK_TAG_H
+#define WECOOK_TAG_H
+
+#include <string>
+#include <vector>
+#include <dart/dynamics/BodyNode.hpp>
+
+namespace wecook {
+
+class Tag {
+ public:
+  Tag(const std::string &tagId,
+         const std::string &objectName,
+         const std::vector<double> &objectPose) : m_tagId(tagId), m_objectName(objectName), m_objectPose(objectPose) {
+
+  }
+
+  std::string getObjectName() const {
+    return m_objectName;
+  }
+
+  std::vector<double> getObjectPose() const {
+    return m_objectPose;
+  }
+
+  std::string getTagId() const {
+    return m_tagId;
+  }
+
+ private:
+  std::string m_tagId;
+  std::string m_objectName;
+  std::vector<double>  m_objectPose;
+
+  // dart::dynamics::BodyNodePtr m_bodyNode = nullptr; PK-TODO: I probably don't need this.
+};
+
+}
+
+#endif //WECOOK_TAG_H
