7d27c28bf1e326d374c3d2929bbf59764aa75b93
Pradeep Kadubandi <pradeep.v.kadubandi@gmail.com>
2020-01-26 18:40:41 -0800

    Code changes for subscribing to april tags info and obtaining the object pose (w.r.t robot base frame) in real world.

--- src/wecook/ObjectMgr.cpp
+++ src/wecook/ObjectMgr.cpp
@@ -5,22 +5,36 @@
 #include <ros/console.h>
 
 #include "wecook/ObjectMgr.h"
+#include "apriltags/AprilTagDetections.h"
 
 using namespace wecook;
 
-void ObjectMgr::init(std::vector<Object> &objects, bool ifSim, aikido::planner::WorldPtr &env) {
+void ObjectMgr::init(std::vector<Object> &objects, std::vector<Tag> &tags, bool ifSim, aikido::planner::WorldPtr &env) {
   for (auto &object : objects) {
     addBodyFromURDF(env.get(), object.getUrl(), object.getPose(), object.getName());
 
-    m_objects.emplace(std::pair<std::string, InternalObject>{object.getName(), InternalObject{ifSim, object, env}});
+    m_objects.emplace(std::pair<std::string, InternalObject>{object.getName(), InternalObject{ifSim, *this, object, env}});
   }
+  for (auto &tag : tags) {
+    m_objectToTagMap[tag.getObjectName()] = tag.getTagId();
+    auto objPose = tag.getObjectPose();
+    auto objTransform = vectorToIsometry(objPose);
+    m_tags.emplace(std::pair<uint32_t, InternalTag>{tag.getTagId(), InternalTag{tag.getTagId(), objTransform}});
+  }
+  m_Listener = m_nh.subscribe("detections", 1000, &ObjectMgr::processTagMsg, this);
 }
 
-void ObjectMgr::clear(std::vector<Object> &objects, aikido::planner::WorldPtr &env) {
+void ObjectMgr::clear(std::vector<Object> &objects, std::vector<Tag> &tags, aikido::planner::WorldPtr &env) {
   for (auto &object : objects) {
     auto skeleton = env->getSkeleton(object.getName());
     env->removeSkeleton(skeleton);
   }
+  // PK-TODO: Do we really need to get tags as input to this method? Can we simple clear the tags and objectToTags dictionaries?
+  for (auto &tag : tags) {
+  }
+  m_tags.clear();
+  m_objectToTagMap.clear();
+  m_Listener.shutdown(); //PK_TODO: Do I need to explicitly call this?
 }
 
 dart::collision::CollisionGroupPtr ObjectMgr::createCollisionGroupExceptFoodAndToMoveObj(const std::string &toMove,
@@ -34,4 +48,23 @@ dart::collision::CollisionGroupPtr ObjectMgr::createCollisionGroupExceptFoodAndT
     envCollisionGroup->addShapeFramesOf(newNode);
   }
   return envCollisionGroup;
+}
+
+void ObjectMgr::processTagMsg(const apriltags::AprilTagDetections::ConstPtr &msg) {
+  for (auto &detection: msg->detections) {
+  // PK-TODO: My lack of knowledge with C++ standard API - there must be a better way to construct the vector below!
+    std::vector<double> vector;
+    vector.push_back(detection.pose.position.x);
+    vector.push_back(detection.pose.position.y);
+    vector.push_back(detection.pose.position.z);
+    vector.push_back(detection.pose.orientation.x);
+    vector.push_back(detection.pose.orientation.y);
+    vector.push_back(detection.pose.orientation.z);
+    vector.push_back(detection.pose.orientation.w);
+    auto camTransform = vectorToIsometry(vector);
+    auto tag = m_tags.find(detection.id);
+    if(tag != m_tags.end()) {
+      tag->second.updateCameraTransform(camTransform);
+    }
+  }
 }
