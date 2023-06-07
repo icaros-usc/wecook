a29337484f3c4232559d2a2aafc3e9a0cf3b1c5a
Hejia Zhang <hjzh578@gmail.com>
2023-06-06 23:43:23 -0700

    Update apritag testing app

--- src/wecook/applications/test_apriltag/src/main.cpp
+++ src/wecook/applications/test_apriltag/src/main.cpp
@@ -8,35 +8,35 @@
 
 int main(int argc, char **argv) {
   Eigen::Isometry3d Tbi = Eigen::Isometry3d::Identity();
-  Tbi.translation() = Eigen::Vector3d(0.91, -0.187, 0.02);
-  Eigen::Matrix3d rot1;
+    Eigen::Matrix3d rot1;
   rot1 <<
        1, 0, 0,
       0, 0, -1,
       0, 1, 0;
+  Tbi.translation() = Eigen::Vector3d(0.91, -0.187, 0.02);
   Tbi.linear() = rot1;
 
   ros::init(argc, argv, "test_apriltag");
   ros::NodeHandle nh("~");
 
   Eigen::Isometry3d Tbc = Eigen::Isometry3d::Identity();
-  Tbc.translation() = Eigen::Vector3d(0.621884348621,
-                                      -0.206405778944,
-                                      1.25697557244);
-  Eigen::Quaterniond rot2(-0.074645740892,
+    Eigen::Quaterniond rot2(-0.074645740892,
                           0.995928692094,
                           -0.0296603146372,
                           0.0409184477553);
+  Tbc.translation() = Eigen::Vector3d(0.621884348621,
+                                      -0.206405778944,
+                                      1.25697557244);
   Tbc.linear() = Eigen::Matrix3d(rot2);
 
   Eigen::Isometry3d Toc = Eigen::Isometry3d::Identity();
-  Toc.translation() = Eigen::Vector3d(-0.249141358159,
-                                      -0.10535720593,
-                                      0.807244164426);
   Eigen::Quaterniond rot3(-0.0885178230092,
                           0.979865228865,
                           -0.044747986937,
                           0.173281118234);
+    Toc.translation() = Eigen::Vector3d(-0.249141358159,
+                                      -0.10535720593,
+                                      0.807244164426);
   Toc.linear() = Eigen::Matrix3d(rot3);
 
   Eigen::Isometry3d Toi = Tbi * Tbc.inverse() * Toc;
